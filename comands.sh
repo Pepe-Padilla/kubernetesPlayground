f42210651aea17fca5065ae08e70a472b0e05155
Jose Padilla <jpadilla@emeal.nttdata.com>
2024-06-26 14:37:48 +0200

    jugando con deployments

--- comands.sh
+++ comands.sh
@@ -5,7 +5,7 @@ minikube status
 minikube start --driver=virtualbox # o el driver que se desé para windows se recomienda hiperv
 
 # Crear un nuevo deployment (create crea un objeto de kubernet) - imperativo
-kubectl create deployment <name> --image="<dockerhub alias>/<cluster image  name>"
+kubectl create deployment <name> --image=<dockerhub alias>/<cluster image  name>
 #### Crea y controla desde un cluster un "mastar node"
 
 # Ver los deployments/pods
@@ -23,3 +23,12 @@ minikube service <nombre imagen>
 # auto Scaling
 kubectl scale deployment/<deployment name> --replicas=3
 kubectl get pods # deberían salir 3 del mismo container+
+
+# Update deployments
+#Cambios en SW la versión es importante pues de otro modo kubernete no verá cambios
+docker build -t <dockerhub alias>/<cluster image  name>:<version> .
+docker push <dockerhub alias>/<cluster image  name>:<version>
+# update <cluster image  name> se puede consultar con: kubectl get pods
+kubectl set image deployment/<deployment name> <cluster image  name>=<dockerhub alias>/<cluster image  name>:<version>
+# para verificar el resultado
+kubectl rollout status deployment/<deployment name>
